f31ae9f04a49c37b88581994d466192f2ce15893
dantcacenco <dantcacenco@gmail.com>
2025-07-22 22:04:29 -0400

    Update Navigation.tsx

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { createClient } from '@/utils/supabase/client'
+import { createClient } from '@/lib/supabase/client'
 import { useRouter } from 'next/navigation'
 
 export default function Navigation() {
