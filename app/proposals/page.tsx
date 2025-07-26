f55f6d41a9c70ccd1800733cd38afa4a44e73c46
Dan Tcacenco <dantcacenco@gmail.com>
2025-07-26 11:53:07 -0400

    force re-deploy

--- app/proposals/page.tsx
+++ app/proposals/page.tsx
@@ -116,7 +116,7 @@ export default async function ProposalsPage({ searchParams }: PageProps) {
     })
   }
 
-  return (
+  return ( 
     <div className="min-h-screen bg-gray-50 py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <ProposalsList 
